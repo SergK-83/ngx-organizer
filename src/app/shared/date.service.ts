a4c0b0a64d7a7a51e3e1cb252732c2dab1becd36
Сергей Крюков <krus-83@mail.ru>
2021-04-01 15:40:11 +0700

    calendar

---
+++ src/app/shared/date.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import * as moment from 'moment';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateService {
+  public date: BehaviorSubject<moment.Moment> = new BehaviorSubject<moment.Moment>(moment());
+
+  changeMonth(dir: number) {
+    const value = this.date.value.add(dir, 'month');
+
+    this.date.next(value);
+  }
+
+  changeDate(day: moment.Moment) {
+    const value = this.date.value.set({
+      date: day.date(),
+      month: day.month()
+    });
+
+    this.date.next(value);
+  }
+}
